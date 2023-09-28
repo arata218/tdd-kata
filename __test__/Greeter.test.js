ae71876740e682a2f2a4d6b7e75fd962e96f012f
arata <arata218@icloud.com>
2023-09-29 01:10:19 +0900

    first kata session

--- __test__/Greeter.test.js
+++ __test__/Greeter.test.js
@@ -4,8 +4,28 @@ describe("Greeter", () => {
   it.each([
     ["Arata", "Hello Arata"],
     ["Yu", "Hello Yu"],
+    // trim
+    ["  Arata  ", "Hello Arata"],
+    ["\tArata", "Hello Arata"],
+    ["\nArata", "Hello Arata"],
+    ["Usami Arata", "Hello Usami Arata"],
+    // capitalize
+    ["arata", "Hello Arata"],
+    ["y", "Hello Y"],
+    ["", "Hello "],
   ])("Says Hello and name", (name, expected) => {
     const greeter = new Greeter();
     expect(greeter.greet(name)).toBe(expected);
+  });
+
+  it("Says Good morning and name", () => {
+    const greeter = new Greeter();
+    jest.useFakeTimers();
+
+    jest.setSystemTime(new Date().setHours(11));
+    expect(greeter.greet("Arata")).toBe("Good morning Arata");
+
+    jest.setSystemTime(new Date().setHours(12));
+    expect(greeter.greet("Arata")).toBe("Hello Arata");
   });
 });
--- src/Greeter.ts
+++ src/Greeter.ts
@@ -1,5 +1,15 @@
 export class Greeter {
   greet(name: string) {
-    return `Hello ${name}`;
+    const trimmedName = name.trim();
+
+    const cap = trimmedName.slice(0, 1).toUpperCase();
+    const rest = trimmedName.slice(1);
+    const capitalizedName = cap + rest;
+
+    const hour = new Date().getHours();
+    if (hour >= 6 && hour < 12) {
+      return `Good morning ${capitalizedName}`;
+    }
+    return `Hello ${capitalizedName}`;
   }
 }
