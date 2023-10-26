6f11df47547b621a8374fdb1292467f06b99f089
arata <arata218@icloud.com>
2023-10-26 17:43:22 +0900

    add test case

--- __test__/StringCalcurator.test.js
+++ __test__/StringCalcurator.test.js
@@ -3,22 +3,39 @@ import { StringCalcurator } from "../src/StringCalcurator";
 describe("StringCalcurator", () => {
   const calc = new StringCalcurator();
 
-  it("returns zero with empty string", () => {
+  test("An empty string returns zero", () => {
     expect(calc.add("")).toBe(0);
   });
 
-  it("returns the value with single number", () => {
+  test("A single number returns the value", () => {
     expect(calc.add("1")).toBe(1);
     expect(calc.add("10")).toBe(10);
   });
 
-  it("returns the sum with comma delimited two numbers", () => {
+  test("Two numbers, comma delimited, returns the sum", () => {
     expect(calc.add("1,2")).toBe(3);
     expect(calc.add("10,20")).toBe(30);
   });
 
-  it("returns the sum with newline delimited two numbers", () => {
+  test("Two numbers, newline delimited, returns the sum", () => {
     expect(calc.add("1\n2")).toBe(3);
     expect(calc.add("10\n20")).toBe(30);
+  });
+
+  test("Three numbers, delimited either way, returns the sum", () => {
+    expect(calc.add("1\n2,3\n4")).toBe(10);
+  });
+
+  test("Negative numbers throw an exception with the message", () => {
+    expect(() => calc.add("-1,2,-3")).toThrow("negatives not allowed: -1,-3");
+  });
+
+  test("Numbers greater than 1000 are ignored", () => {
+    expect(calc.add("1000,1")).toBe(1001);
+    expect(calc.add("1001,1")).toBe(1);
+  });
+
+  test("A single char delimiter can be defined on the first line starting with //", () => {
+    expect(calc.add("//#\n1#2")).toBe(3);
   });
 });
--- src/StringCalcurator.ts
+++ src/StringCalcurator.ts
@@ -1,10 +1,28 @@
 export class StringCalcurator {
   add(str: String) {
-    const arr = str.split(/[,\n]/);
+    let arr = [];
+
+    if (str.startsWith("//")) {
+      const delimiter = str[2];
+      arr = str.slice(3).split(/[,\n#{delimiter}]/);
+    } else {
+      arr = str.split(/[,\n]/);
+    }
 
     let sum = 0;
+    const negatives = [];
+
     for (const elm of arr) {
-      sum += Number(elm);
+      const num = Number(elm);
+      if (num > 1000) continue;
+      if (num < 0) {
+        negatives.push(num);
+      }
+      sum += num;
+    }
+
+    if (negatives.length !== 0) {
+      throw new Error(`negatives not allowed: ${negatives.join(",")}`);
     }
 
     return sum;
