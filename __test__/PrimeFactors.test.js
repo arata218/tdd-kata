a86fdef9eef54d70864de083ef3b420d026436a6
arata <arata218@icloud.com>
2023-11-09 16:01:02 +0900

    fix testcase string

--- __test__/PrimeFactors.test.js
+++ __test__/PrimeFactors.test.js
@@ -39,7 +39,7 @@ describe("PrimeFactors", () => {
     expect(pf.generate(9)).toStrictEqual([3, 3]);
   });
 
-  test("4620 should return [2,2,3,5,7,11]", () => {
+  test("4620 should return [2, 2, 3, 5, 7, 11]", () => {
     expect(pf.generate(4620)).toStrictEqual([2, 2, 3, 5, 7, 11]);
   });
 });
