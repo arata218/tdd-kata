a07e13366a18a1683c7524a76d4e21499ea08975
arata <arata218@icloud.com>
2023-11-14 13:17:09 +0900

    fix conditional expression

--- src/PrimeFactors.ts
+++ src/PrimeFactors.ts
@@ -3,7 +3,7 @@ export class PrimeFactors {
     const factors = [];
     let temp = num;
 
-    for (let div = 2; div <= num; div++) {
+    for (let div = 2; temp > 1; div++) {
       while (temp % div === 0) {
         factors.push(div);
         temp /= div;
