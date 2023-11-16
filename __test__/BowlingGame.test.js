40abd494093ea5d0d625c84ce715bb9d7a0be2b4
arata <arata218@icloud.com>
2023-11-16 17:18:38 +0900

    update BowlingGame

--- __test__/BowlingGame.test.js
+++ __test__/BowlingGame.test.js
@@ -6,7 +6,46 @@ describe("BowlingGame", () => {
   beforeEach(() => {
     game = new BowlingGame();
   });
-  test("new game", () => {
+
+  test("New game", () => {
     expect(game.getScore()).toBe(0);
   });
+
+  test("Roll and getScore", () => {
+    game.roll(1);
+    expect(game.getScore()).toBe(1);
+  });
+
+  test("Roll impossible value returns error", () => {
+    expect(() => game.roll(11)).toThrow("impossible value: 11");
+    expect(() => game.roll(-1)).toThrow("impossible value: -1");
+  });
+
+  test("1st frame", () => {
+    game.roll(1);
+    game.roll(1);
+    expect(game.getScore()).toBe(2);
+  });
+
+  test("2nd frame", () => {
+    game.roll(1);
+    game.roll(1);
+    game.roll(2);
+    game.roll(2);
+    expect(game.getScore()).toBe(6);
+  });
+
+  test("Spare", () => {
+    game.roll(5);
+    game.roll(5);
+    game.roll(1);
+    game.roll(1);
+    expect(game.getScore()).toBe(13);
+  });
+
+  test("Strike", () => {});
+
+  test("Turkey", () => {});
+
+  test("10th frame", () => {});
 });
--- src/BowlingGame.ts
+++ src/BowlingGame.ts
@@ -1,15 +1,41 @@
+type Frame = {
+  first: number;
+  second: number;
+};
+
 export class BowlingGame {
   constructor() {
-    this.score = 0;
-    this.bonus = 0;
+    this.frames = [];
+    this.throwed = false;
   }
 
-  score: number;
-  bonus: number;
+  frames: Frame[];
+  throwed: boolean;
 
-  roll(pins: number) {}
+  roll(pins: number) {
+    if (pins < 0 || pins > 10) {
+      throw new Error(`impossible value: ${pins}`);
+    }
+    if (!this.throwed) {
+      const frame: Frame = { first: pins, second: 0 };
+      this.frames.push(frame);
+      this.throwed = true;
+    } else {
+      const frame: Frame = this.frames.pop() as Frame;
+      frame.second = pins;
+      this.frames.push(frame);
+      this.throwed = false;
+    }
+  }
 
   getScore() {
-    return this.score;
+    let totalScore = 0;
+
+    for (const f of this.frames) {
+      const score = f.first + f.second;
+      totalScore += score;
+    }
+
+    return totalScore;
   }
 }
