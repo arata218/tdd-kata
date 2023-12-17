27e4ba4ae77cb4cdc5c1ed48b64c66bdd85230b3
arata <arata218@icloud.com>
2023-12-17 14:25:49 +0900

    delete bonus property

--- src/BowlingGame.ts
+++ src/BowlingGame.ts
@@ -1,17 +1,13 @@
 type Frame = number[];
 
-type Bonus = "strike" | "spare" | "none";
-
 export class BowlingGame {
   constructor() {
     this.frames = [];
     this.filled = true;
-    this.bonus = [];
   }
 
   frames: Frame[];
   filled: boolean;
-  bonus: Bonus[];
 
   roll(pins: number) {
     if (this.frames.length === 10 && this.filled) {
@@ -23,45 +19,41 @@ export class BowlingGame {
     }
 
     if (this.filled) {
-      const frame: Frame = [pins];
-      this.frames.push(frame);
+      const f: Frame = [pins];
+      this.frames.push(f);
 
       if (pins === 10) {
-        this.bonus.push("strike");
-        if (this.bonus.length === 10) this.filled = false;
+        if (this.frames.length === 10) this.filled = false;
         return;
       }
 
       this.filled = false;
     } else {
-      const frame: Frame = this.frames.pop() as Frame;
-      frame.push(pins);
+      const f: Frame = this.frames.pop() as Frame;
+      f.push(pins);
 
-      if (this.bonus[9] === "strike") {
-        if (frame[1] !== 10 && frame[1] + frame[2] > 10) {
+      if (f[0] === 10) {
+        if (f[1] !== 10 && f[1] + f[2] > 10) {
           throw new Error("Over 10 pins");
         }
 
-        this.frames.push(frame);
-        if (frame.length === 3) this.filled = true;
+        this.frames.push(f);
+        if (f.length === 3) this.filled = true;
         return;
       }
 
-      if (frame[0] + frame[1] > 10) {
+      if (f[0] + f[1] > 10) {
         throw new Error("Over 10 pins");
       }
 
-      this.frames.push(frame);
-      if (frame.length === 3) {
+      this.frames.push(f);
+      if (f.length === 3) {
         this.filled = true;
         return;
       }
 
-      if (frame[0] + frame[1] === 10) {
-        this.bonus.push("spare");
-        if (this.bonus.length === 10) return;
-      } else {
-        this.bonus.push("none");
+      if (f[0] + f[1] === 10) {
+        if (this.frames.length === 10) return;
       }
 
       this.filled = true;
@@ -71,28 +63,21 @@ export class BowlingGame {
   getScore() {
     let totalScore = 0;
 
-    for (const f of this.frames) {
+    this.frames.forEach((f, i) => {
       totalScore += f.reduce((sum, elm) => sum + elm);
-    }
 
-    this.bonus.forEach((b, i) => {
       if (i == 9) return;
 
-      switch (b) {
-        case "strike":
-          if (i !== 8 && this.frames[i + 1][0] === 10) {
-            totalScore += 10 + this.frames[i + 2][0];
-          } else {
-            totalScore += this.frames[i + 1][0] + this.frames[i + 1][1];
-          }
-          break;
+      if (f[0] === 10) {
+        if (i !== 8 && this.frames[i + 1][0] === 10) {
+          totalScore += 10 + this.frames[i + 2][0];
+        } else {
+          totalScore += this.frames[i + 1][0] + this.frames[i + 1][1];
+        }
+      }
 
-        case "spare":
-          totalScore += this.frames[i + 1][0];
-          break;
-
-        case "none":
-          break;
+      if (f[0] + f[1] === 10) {
+        totalScore += this.frames[i + 1][0];
       }
     });
 
