c0dda787d39784b3f4293f47179eafeda7190510
arata <arata218@icloud.com>
2023-12-24 14:42:00 +0900

    add fixtures

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -1,16 +1,27 @@
+type Rule = { value: number; word: string };
+
 export class FizzBuzz {
-  constructor() {}
+  constructor(r: Rule[]) {
+    this.rules = [
+      { value: 3, word: "Fizz" },
+      { value: 5, word: "Buzz" },
+    ];
+    this.rules.concat(r);
+  }
+
+  rules: Rule[];
 
   generate(from: number = 1, to: number = 100) {
     let arr: (number | string)[] = [];
+    let str: string = "";
 
     for (let i = from; i <= to; i += 1) {
-      if (i % 15 === 0) {
-        arr.push("FizzBuzz");
-      } else if (i % 3 === 0) {
-        arr.push("Fizz");
-      } else if (i % 5 === 0) {
-        arr.push("Buzz");
+      if (i % (this.rules[0].value * this.rules[1].value) === 0) {
+        arr.push(this.rules[0].word + this.rules[1].word);
+      } else if (i % this.rules[0].value === 0) {
+        arr.push(this.rules[0].word);
+      } else if (i % this.rules[1].value === 0) {
+        arr.push(this.rules[1].word);
       } else {
         arr.push(i);
       }
